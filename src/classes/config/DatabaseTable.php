d4072a82ca243de5168a1107025cbbcc55ac8d61
anastasija-stepanova <nastyaaa19980@gmail.com>
2017-07-13 09:46:30 +0300

    Добавлен файл DatabaseTable.php

---
+++ src/classes/config/DatabaseTable.php
@@ -0,0 +1,9 @@
+<?php
+class DatabaseTable
+{
+    const USER = 'user';
+    const RAW_DATA = 'raw_data';
+    const TEST_INFO = 'test_info';
+    const USER_URLS = 'user_urls';
+    const AVERAGE_RESULT = 'average_result';
+}
