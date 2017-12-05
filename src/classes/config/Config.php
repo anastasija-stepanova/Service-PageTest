17adf5637a1980f7f517d75ae4e424727d3cfba8
anastasija-stepanova <nastyaaa19980@gmail.com>
2017-12-05 12:16:44 +0300

    Изменены данные для доступа к базе данных

--- src/classes/config/Config.php
+++ src/classes/config/Config.php
@@ -1,9 +1,9 @@
 <?php
 class Config
 {
-    const MYSQL_HOST = 'localhost';
-    const MYSQL_USERNAME = 'root';
-    const MYSQL_PASSWORD = 'qwerty';
-    const MYSQL_DATABASE = 'page_load_statistics';
+    const MYSQL_HOST = 'us-cdbr-iron-east-05.cleardb.net';
+    const MYSQL_USERNAME = 'b4139a4ec9f63e';
+    const MYSQL_PASSWORD = '7e25954f';
+    const MYSQL_DATABASE = 'heroku_4abc7dd611fc4b9';
     const MYSQL_PORT = '3306';
 }
